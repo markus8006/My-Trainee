e60b725487aca5a1a189dbcf18750374ac3f82b3
markus8006 <guilhermemarkus8@gmail.com>
2025-06-23 19:51:12 -0300

    Reorganizando

--- Trainee/vozes.py
+++ Trainee/vozes.py
@@ -16,42 +16,84 @@ voices = [
     "Elevenlabs"
 ]
 
+class Voices:
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.config = json_config.ler_configuracao(nome)
+        self.modelo = self.config["Voices"]['model']
+        self.api = self.config["Voices"]["API_VOICE"]
+        self.engine = None
+        self.client = None
+
+        if self.modelo in voices:
+            if self.modelo == 'Win':
+                pass
+            elif self.modelo == 'Elevenlabs':
+                pass
+    
+
+    def _init_elevenlabs(self):
+        if config.DEBUG:
+            log.executando('Acessando client Elevenlabs...')
+        self.client = ElevenLabs(api_key=self.api)
+        if config.DEBUG:
+            log.sucess("Client criado")
 
 
-def configurar_elevenlabs(api, nome):
-    'configura os elementos do elevenlabs'
 
-    global client
-    if config.DEBUG:
-        log.executando("Criando cliente Elevenlabs...")
-    client = ElevenLabs(api_key=api)
-    if config.DEBUG:
-        log.sucess("Client criado")
-        log.executando("Salvando mudanças")
 
-    arquivo = json_config.ler_configuracao(nome)
-    arquivo['Voice']['model'] = "Elevenlabs"
-    arquivo["Voice"]['API_VOICE'] = api
-    json_config.salvar_arquivo(nome, arquivo)
-    if config.DEBUG:
-        log.sucess("Alteações salvas")
-
+    def configurar_elevenlabs(self, nova_api : str, 
+                              voice_id = "qbzdNKUUxcKHZ1WhQ7eX", 
+                              model_id = "eleven_multilingual_v2"):
+        
+        'configura os elementos do elevenlabs'
+        self.api = nova_api
+        self.client = ElevenLabs(api_key=nova_api)
+        self.config['Voice']['model'] = "Elevenlabs"
+        self.config['Voice']['API_VOICE'] = nova_api
+        json_config.salvar_arquivo(self.nome, self.config)
+        if config.DEBUG:
+            log.sucess("Nova configuração salva")
+       
     
 
 
-def falar(texto: str):
+    def falar(self, texto: str):
 
-    'usa a fala em audio'
-    if config.DEBUG:
-        log.executando("Acessando modelo elevenlabs...")
-    audio = b"".join(client.text_to_speech.convert(
-    text=texto,
-    voice_id="qbzdNKUUxcKHZ1WhQ7eX",
-    model_id="eleven_multilingual_v2",
-    output_format="pcm_22050",
-    ))
-    if config.DEBUG:
-        log.sucess("Elevenlabs acessado")
-        audio_array = np.frombuffer(audio, dtype=np.int16)
-        sd.play(audio_array, samplerate=22050)
-        sd.wait()
+        if self.modo == "Win":
+            self.engine.say(texto)
+            self.engine.runAndWait()
+
+
+        elif self.modo == "Elevenlabs":
+            
+
+            audio_array = self._run_audio_elevenlabs(texto)
+            sd.play(audio_array, samplerate=22050)
+            sd.wait()
+
+            if config.DEBUG:
+                log.sucess("Áudio reproduzido.")
+
+
+
+
+
+
+
+
+
+    def _run_audio_elevenlabs(self, texto):
+        if config.DEBUG:
+                log.executando("Acessando modelo ElevenLabs...")
+
+        audio = b''.join(
+            self.client.text_to_speech.convert(
+            text=texto,
+            voice_id="qbzdNKUUxcKHZ1WhQ7eX",
+            model_id="eleven_multilingual_v2",
+            output_format="pcm_22050",
+                )
+            )
+        return np.frombuffer(audio, dtype=np.int16)
